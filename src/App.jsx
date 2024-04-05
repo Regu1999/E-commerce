7bb8c08151fdc683365eb5703f15a346be2f65d9
Regu1999 <reguramachandran007@gmail.com>
2024-04-05 16:51:28 +0530

    extra home page added in app page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Shop from "./Component/Shop.jsx";
 import NavBar from "./Component/Navbar.jsx";
+import Home from "./Component/Home.jsx";
 import Cart from "./Component/cart/Cart.jsx";
 export default function App() {
   return (
     <div>
       <NavBar />
       {/* <Cart /> */}
+      {/* <Home /> */}
       <Shop />
     </div>
     )
