315461b4f452336a3ea00a6714f7d2c6ecfcf270
Regu1999 <reguramachandran007@gmail.com>
2024-04-24 18:56:00 +0530

    fillter ui update and added to the shop

--- src/Component/Filter.jsx
+++ src/Component/Filter.jsx
@@ -1,10 +1,31 @@
 export default function Filter() {
-    return <div className="p-3">
-        <h5>Filter</h5>
-        <div>
-            <label>Size</label>
-            <label htmlFor=""></label>
-            <label>Price</label>
+    const CheckBox = ({ inputName }) => {
+        return <div className="">
+            <input type="checkbox" name="" id={inputName} className="me-1" />
+            <label htmlFor={inputName}>{inputName}</label>
         </div>
+    }
+    return <div className="p-3">
+        {/* <h5>Filter</h5> */}
+        {/* <div> */}
+        <label>Size</label>
+        <section className="grid grid-cols-3 gap-1 min-w-44 mb-3">
+            <CheckBox inputName='XS' />
+            <CheckBox inputName='S' />
+            <CheckBox inputName='M' />
+            <CheckBox inputName='ML' />
+            <CheckBox inputName='L' />
+            <CheckBox inputName='XL' />
+            <CheckBox inputName='XXL' />
+        </section>
+        <label>Price $</label>
+        <section className="grid grid-cols-2 gap-1">
+            <CheckBox inputName='5-20' />
+            <CheckBox inputName='20-40' />
+            <CheckBox inputName='40-60' />
+            <CheckBox inputName='50-100' />
+        </section>
+
+        {/* </div> */}
     </div>
 }
--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -2,6 +2,8 @@ import Product from "../Component/Product.jsx"
 import Filter from "../Component/Filter.jsx";
 import { json, useLoaderData } from "react-router-dom";
 import { useMediaQueryDevice } from "../hooks/useMediaQuryDevice.js";
+import { IoCloseSharp } from "react-icons/io5";
+
 export default function Shop() {
     const { isTablet } = useMediaQueryDevice()
     const loaderData = useLoaderData();
@@ -10,7 +12,14 @@ export default function Shop() {
     return (
         <div className="flex animate-fade-in h-full">
             {isTablet && <div className="border-r shadow-md w-[900px]"><Filter /></div>}
-            <div className="flex flex-wrap justify-center">
+            <div className="flex flex-wrap justify-center p-3">
+                <div className="w-full">
+                    <p className=" text-xl mb-2">Your selection</p>
+                    <span className="shadow shadow-rose-200 p-2 rounded-full cursor-pointer">
+                        <label htmlFor="">Summa</label>
+                        <button className="bg-black h-5 w-5 rounded-full text-white"><IoCloseSharp /></button>
+                    </span>
+                </div>
                 {products.map((product) => {
                     return <Product key={product.id} product={product} />
                 })}
