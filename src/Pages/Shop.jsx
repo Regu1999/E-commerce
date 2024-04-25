534c25efe26d55d477d2e2db36aa5b146f8a2097
Regu1999 <reguramachandran007@gmail.com>
2024-04-25 16:57:53 +0530

    selection ui added

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -15,10 +15,23 @@ export default function Shop() {
             <div className="flex flex-wrap justify-center p-3">
                 <div className="w-full">
                     <p className=" text-xl mb-2">Your selection</p>
-                    <span className="shadow shadow-rose-200 p-2 rounded-full cursor-pointer">
-                        <label htmlFor="">Summa</label>
-                        <button className="bg-black h-5 w-5 rounded-full text-white"><IoCloseSharp /></button>
+                    <span className="shadow shadow-gray-500 text-sm font-bold bg-rose-200 text-rose-100 p-1 px-2 rounded-full cursor-pointer text-center inline-flex items-center gap-1 me-3 mb-3 hover:scale-105">
+                        <label htmlFor="">XS</label>
+                        <button className="bg-rose-100 h-4 w-4 rounded-full text-rose-200 inline-flex justify-center items-center hover:bg-black"><IoCloseSharp /></button>
                     </span>
+                    <span className="shadow shadow-gray-500 text-sm font-bold bg-rose-200 text-rose-100 p-1 px-2 rounded-full cursor-pointer text-center inline-flex items-center gap-1 me-3 mb-3 hover:scale-105">
+                        <label htmlFor="">5-20</label>
+                        <button className="bg-rose-100 h-4 w-4 rounded-full text-rose-200 inline-flex justify-center items-center hover:bg-black"><IoCloseSharp /></button>
+                    </span>
+                    <span className="shadow shadow-gray-500 text-sm font-bold bg-rose-200 text-rose-100 p-1 px-2 rounded-full cursor-pointer text-center inline-flex items-center gap-1 me-3 mb-3 hover:scale-105">
+                        <label htmlFor="">XXl</label>
+                        <button className="bg-rose-100 h-4 w-4 rounded-full text-rose-200 inline-flex justify-center items-center hover:bg-black"><IoCloseSharp /></button>
+                    </span>
+                    <span className="shadow shadow-gray-500 text-sm font-bold bg-rose-200 text-rose-100 p-1 px-2 rounded-full cursor-pointer text-center inline-flex items-center gap-1 me-3 mb-3 hover:scale-105">
+                        <label htmlFor="">50-100</label>
+                        <button className="bg-rose-100 h-4 w-4 rounded-full text-rose-200 inline-flex justify-center items-center hover:bg-black"><IoCloseSharp /></button>
+                    </span>
+                    <hr className="mt-3"/>
                 </div>
                 {products.map((product) => {
                     return <Product key={product.id} product={product} />
