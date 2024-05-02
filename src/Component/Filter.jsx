8853c7fa3b9a7f574851898402df356c7a153956
Regu1999 <reguramachandran007@gmail.com>
2024-05-02 12:58:15 +0530

    filter pages change

--- src/Component/Filter.jsx
+++ src/Component/Filter.jsx
@@ -1,22 +1,17 @@
+const CheckBox = ({ inputName }) => {
+    return <div className="">
+        <input type="checkbox" name={inputName} id={inputName} className="me-1" />
+        <label htmlFor={inputName} className="select-none">{inputName}</label>
+    </div>
+}
+const availabelSize=['XS','S','M','ML','L','XL','XXL'];
 export default function Filter() {
-    const CheckBox = ({ inputName }) => {
-        return <div className="">
-            <input type="checkbox" name="" id={inputName} className="me-1" />
-            <label htmlFor={inputName}>{inputName}</label>
-        </div>
-    }
     return <div className="p-3">
         {/* <h5>Filter</h5> */}
         {/* <div> */}
         <label>Size</label>
         <section className="grid grid-cols-3 gap-1 min-w-44 mb-3">
-            <CheckBox inputName='XS' />
-            <CheckBox inputName='S' />
-            <CheckBox inputName='M' />
-            <CheckBox inputName='ML' />
-            <CheckBox inputName='L' />
-            <CheckBox inputName='XL' />
-            <CheckBox inputName='XXL' />
+            {availabelSize.map(size=> <CheckBox inputName={size} key={size} />)}
         </section>
         <label>Price $</label>
         <section className="grid grid-cols-2 gap-1">
