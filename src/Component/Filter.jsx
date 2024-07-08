f3401c56e1630f75bb92395d6a238b6457150d36
Regu1999 <reguramachandran007@gmail.com>
2024-07-08 13:21:22 +0530

    Chore: code changed for better readable

--- src/Component/Filter.jsx
+++ src/Component/Filter.jsx
@@ -7,20 +7,18 @@ const CheckBox = ({ inputName }) => {
 }
 const availabelSize = ['XS', 'S', 'M', 'ML', 'L', 'XL', 'XXL'];
 export default function Filter() {
-    const proceRange = splitPriceRange(0,100,20);
-    console.log(proceRange);
+    const startPrice = 0;
+    const maxPrice = 100;
+    const range = 20;
+    const proceRange = splitPriceRange(startPrice, maxPrice, range);
     return <div className="p-3">
-        {/* <h5>Filter</h5> */}
-        {/* <div> */}
         <label>Size</label>
         <section className="grid grid-cols-3 gap-1 min-w-44 mb-3">
             {availabelSize.map(size => <CheckBox inputName={size} key={size} />)}
         </section>
         <label>Price $</label>
         <section className="grid grid-cols-2 gap-1">
-            {proceRange.map(price=><CheckBox inputName={price} key={price} />)}
+            {proceRange.map(price => <CheckBox inputName={price} key={price} />)}
         </section>
-
-        {/* </div> */}
     </div>
 }
