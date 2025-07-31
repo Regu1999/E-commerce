f36446552ccb596f7311c18b6b99d0421860832f
Regu1999 <reguramachandran007@gmail.com>
2025-07-31 16:36:59 +0530

    testing

--- backend/api/app.js
+++ backend/api/app.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.static('images'));
 app.use(bodyParser.json());
 app.use(cookieParser())
-const CROS_ORIGIN_URL = process.env.CROS_ORIGIN_URL || "*";
+const CROS_ORIGIN_URL = process.env.CROS_ORIGIN_URL;
 
 app.use(cors({
   origin: CROS_ORIGIN_URL,
