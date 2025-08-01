8178098b850a9927d8a84f95faf2c4f79df6e042
Regu1999 <reguramachandran007@gmail.com>
2025-08-01 13:06:12 +0530

    image location changed

--- backend/api/app.js
+++ backend/api/app.js
@@ -25,7 +25,7 @@ app.use(cors({
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use('/images', express.static(path.join(__dirname, 'public/images')));
+app.use('/images', express.static('images'));
 
 app.use(productRoute)
 app.use(authRout);
