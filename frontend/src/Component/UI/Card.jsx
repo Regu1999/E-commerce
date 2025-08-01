9338edd39f0f644ba2ed06eb57cd322f817c77f8
Regu1999 <reguramachandran007@gmail.com>
2025-08-01 18:03:14 +0530

    Cart.jsx modified

--- frontend/src/Component/UI/Card.jsx
+++ frontend/src/Component/UI/Card.jsx
@@ -1,5 +1,5 @@
 export default function Card({ children, shadow = true, styleClass }) {
-    return <div className={`w-96 min-h-96 h-full bg-white p-5 sm:p-14 rounded-md my-5 ${shadow && 'shadow-2xl'} ${styleClass}`}>
+    return <div className={`w-96 min-h-96 h-full mx-5 bg-white p-5 sm:p-14 rounded-md my-5 ${shadow && 'shadow-2xl'} ${styleClass}`}>
         {children}
     </div>
 }
